0827c68187c3e82029161d1de3eecfc5c485e8e7
Algello <36562479+Algello@users.noreply.github.com>
2018-08-20 21:51:12 +0400

    Starting to explore BigO Notation
    Added 2 functions

--- BigO_Notation/main.js
+++ BigO_Notation/main.js
@@ -1,1 +1,13 @@
-console.log('hello from bigO');
+document.addEventListener("DOMContentLoaded", function(event) {
+    function addUpTo(n) {
+        let total = 0;
+        for (let i = 0; i <= n; i++) {
+            total += i;
+        }
+        return total;
+    }
+    function addUpTo2(n) {
+
+    }
+    console.log(addUpTo(6));
+});
